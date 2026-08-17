07fff8015b7d202dc97fc13c193d59038be5f19f
sanyaa404 <sanya.gera01@gmail.com>
2026-08-18 01:28:48 +0530

    Time: 0 ms (100%), Space: 53.4 MB (11.51%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,38 @@
+//dp
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        if(n==1 || n==0) return true;
-        int lst = 0;
-        for(int i=0; i<n-1; i++){
-            if(lst < i) return false;
-            lst = max(lst, nums[i]+i);
-            if(lst >= n-1) return true;
+        vector<bool> dp(n, false);
+        int lastTrueIdx = n-1;
+        for (int i = n-1; i >= 0; i--) {
+            if (i + nums[i] >= n - 1) {
+                dp[i] = true;
+                lastTrueIdx = i;
+            }
+        
+            if (i + nums[i] >= lastTrueIdx) {
+                dp[i] = true;
+                lastTrueIdx = i;
+            }
         }
-        return false;
+
+        return dp[0];
     }
 };
+
+//greedy
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n==1 || n==0) return true;
+//         int lst = 0;
+//         for(int i=0; i<n-1; i++){
+//             if(lst < i) return false;
+//             lst = max(lst, nums[i]+i);
+//             if(lst >= n-1) return true;
+//         }
+//         return false;
+//     }
+// };
