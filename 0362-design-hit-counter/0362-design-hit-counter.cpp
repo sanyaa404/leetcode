9c9787081b760ab2e387347c92009008e8d35a3a
sanyaa404 <sanya.gera01@gmail.com>
2026-08-24 20:55:37 +0530

    Time: 0 ms (100%), Space: 10.5 MB (91.71%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.cpp
+++ 0362-design-hit-counter/0362-design-hit-counter.cpp
@@ -1,19 +1,46 @@
+//design for scaling
 class HitCounter {
 public:
-    queue<int> q;
+    vector<pair<int, int>> v;
     HitCounter() {
-        
+        v.resize(300, {0,0});
     }
     
     void hit(int timestamp) {
-        q.push(timestamp);
+        int t = timestamp%300;
+        if(v[t].first == timestamp){
+            v[t].second++;
+        }else{
+            v[t] = {timestamp, 1};
+        }
     }
     
     int getHits(int timestamp) {
-        while(!q.empty() && timestamp - q.front() >= 300) q.pop();
-        return q.size();
+        int ans = 0;
+        for(int i=0; i<300; i++){
+            if(timestamp - v[i].first < 300) ans += v[i].second;
+        }
+        return ans;
     }
 };
+
+//normal soln
+// class HitCounter {
+// public:
+//     queue<int> q;
+//     HitCounter() {
+        
+//     }
+    
+//     void hit(int timestamp) {
+//         q.push(timestamp);
+//     }
+    
+//     int getHits(int timestamp) {
+//         while(!q.empty() && timestamp - q.front() >= 300) q.pop();
+//         return q.size();
+//     }
+// };
 
 /**
  * Your HitCounter object will be instantiated and called as such:
