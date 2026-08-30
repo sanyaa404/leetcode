052ce7ef3fcf20c3a63d8370fb2cfb39000c2efe
sanyaa404 <sanya.gera01@gmail.com>
2026-08-30 17:50:11 +0530

    Time: 4 ms (61.65%), Space: 12.1 MB (66.65%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool solve(int i, int j, int n, string &s, unordered_set<string> &words, vector<vector<int>> &dp){
-        if(i>j){
+    bool solve(int i, int n, string &s, unordered_set<string> &words, vector<int> &dp){
+        if(i>=n){
             return true;
         }
-        if(dp[i][j] != -1) return dp[i][j];
+        if(dp[i] != -1) return dp[i];
         string word = "";
         int ans = 0;
-        for(int k=i; k<=j; k++){
+        for(int k=i; k<=n; k++){
             word += s[k];
             //add partition
             if(words.find(word) != words.end()){
-                ans = ans || solve(k+1,n-1, n, s, words, dp);
+                ans = ans || solve(k+1, n, s, words, dp);
             }
         }
-        return dp[i][j] = ans;
+        return dp[i] = ans;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> words(wordDict.begin(), wordDict.end());
         int n = s.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-        return solve(0, n-1, n, s, words, dp);
+        vector<int> dp(n,-1);
+        return solve(0, n, s, words, dp);
     }
 };
