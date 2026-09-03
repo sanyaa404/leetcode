7521a077554e3cc688207b4b0582578da4115ec0
sanyaa404 <sanya.gera01@gmail.com>
2026-09-03 13:29:28 +0530

    Time: 8 ms (97.74%), Space: 48 MB (88.88%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,72 +1,66 @@
-struct DLListNode {
+class DListNode{
+public:
     int val;
-    DLListNode *next;
-    DLListNode *prev;
-    DLListNode() : val(0), next(nullptr), prev(nullptr) {}
-    DLListNode(int x) : val(x), next(nullptr), prev(nullptr) {}
-    DLListNode(int x, DLListNode *next, DLListNode *prev) : val(x), next(next), prev(prev) {}
+    DListNode* next;
+    DListNode* prev;
+    DListNode(): val(0), next(nullptr), prev(nullptr) {}
+    DListNode(int val): val(val), next(nullptr), prev(nullptr) {}
+    DListNode(int x, DListNode *next, DListNode *prev) : val(x), next(next), prev(prev) {}
 };
+
 class LRUCache {
 public:
-    int capacity;
-
-    DLListNode* head;
-    DLListNode* tail;
-
-    unordered_map<int, pair<DLListNode*, int>> mp;
-
-    LRUCache(int n) {
-        capacity = n;
-        head = new DLListNode();
-        tail = new DLListNode();
-        head->next = tail;
-        tail->prev = head;
+    int n;
+    unordered_map<int, pair<DListNode*, int>> mp;
+    DListNode* head;
+    DListNode* tail;
+    LRUCache(int capacity) {
+        head = new DListNode();
+        tail = new DListNode();
+        n = capacity;
+        head->next = tail; tail->prev = head;
     }
-
-    void remove(DLListNode* node) {
-        node->prev->next = node->next;
-        node->next->prev = node->prev;
-    }
-
-    void putlast(DLListNode* node) {
-        node->prev = tail->prev;
-        node->next = tail;
-
-        tail->prev->next = node;
-        tail->prev = node;
-    }
-
+    
     int get(int key) {
-        if (mp.find(key) == mp.end())
-            return -1;
-
-        DLListNode* node = mp[key].first;
-        int value = mp[key].second;
-        remove(node);
-        putlast(node);
-        return value;
+        if(mp.find(key) == mp.end()) return -1;
+        detach(mp[key].first);
+        putLast(mp[key].first);
+        return mp[key].second;
     }
 
+    void detach(DListNode* node){
+        DListNode* prev = node->prev; DListNode* next = node->next;
+        node->prev = NULL; node->next = NULL;
+        next->prev = prev;
+        prev->next = next;
+    }
+
+    void putLast(DListNode* node){
+        DListNode* prev = tail->prev;
+        prev->next = node; node->prev = prev;
+        node->next = tail; tail->prev = node;
+    }
+    
     void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
-            DLListNode* node = mp[key].first;
+        if(mp.find(key) != mp.end()){
+            detach(mp[key].first);
+            putLast(mp[key].first);
             mp[key].second = value;
-            remove(node);
-            putlast(node);
             return;
         }
 
-        // Cache full → remove LRU
-        if (mp.size() == capacity) {
-            DLListNode* node = head->next;
-            remove(node);
-            mp.erase(node->val);
-            delete node;
+        DListNode* newnode = new DListNode(key);
+        mp[key] = {newnode, value};
+        putLast(newnode);
+        if(n > 0){
+            n--;
+        }else{
+            int keyToRemove = head->next->val;
+            DListNode* nodeToRemove = head->next;
+            detach(nodeToRemove);
+            mp.erase(keyToRemove);
+            delete nodeToRemove;
         }
-
-        DLListNode* node = new DLListNode(key);
-        putlast(node);
-        mp[key] = {node, value};
     }
 };
 
