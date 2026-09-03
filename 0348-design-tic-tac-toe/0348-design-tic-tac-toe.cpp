c9838c3a8e6aacb7852f25b0872230185484b6f7
sanyaa404 <sanya.gera01@gmail.com>
2026-09-03 14:59:20 +0530

    Time: 16 ms (86.74%), Space: 26.4 MB (14.85%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
@@ -1,54 +1,49 @@
 class TicTacToe {
 public:
-    vector<vector<int>> board;
+    vector<pair<int, int>> rowCnt;
+    vector<pair<int, int>> colCnt;
+    vector<pair<int, int>> diagCnt;
     int n;
     TicTacToe(int sze) {
         n = sze;
-        board.resize(n, vector<int>(n,0));
+        // board.resize(n, vector<int>(n,0));
+        rowCnt.resize(n, {0,0});
+        colCnt.resize(n, {0,0});
+        diagCnt.resize(2, {0,0});
     }
 
     bool checkRow(int row, int player){
-        for(int j=0; j<n; j++){
-            if(board[row][j] != player) return false;
-        } return true;
+        if(player == 2) return (rowCnt[row].second == n);
+        return (rowCnt[row].first == n);
     }
     
     bool checkCol(int col, int player){
-        for(int i=0; i<n; i++){
-            if(board[i][col] != player) return false;
-        } return true;
+        if(player == 2) return (colCnt[col].second == n);
+        return (colCnt[col].first == n);
     }
 
-    bool checkDiag(int row, int col, int player) {
-        if(row == col) {
-            bool ok = true;
-            for(int i = 0; i < n; i++) {
-                if(board[i][i] != player) {
-                    ok = false;
-                    break;
-                }
-            }
-            if(ok) return true;
+    bool checkDiag(int player) {
+        if(player == 2){
+            return (diagCnt[0].second == n) || (diagCnt[1].second == n);
         }
-        if(row + col == n - 1) {
-            bool ok = true;
-
-            for(int i = 0; i < n; i++) {
-                if(board[i][n - 1 - i] != player) {
-                    ok = false;
-                    break;
-                }
-            }
-            if(ok) return true;
-        }
-        return false;
+        return (diagCnt[0].first == n) || (diagCnt[1].first == n);
     }
     
     int move(int row, int col, int player) {
-        board[row][col] = player;
+        if(player == 1){
+            rowCnt[row].first++;
+            colCnt[col].first++;
+            if(row == col) diagCnt[0].first++;
+            if(row + col == n-1) diagCnt[1].first++;
+        }else{
+            rowCnt[row].second++;
+            colCnt[col].second++;
+            if(row == col) diagCnt[0].second++;
+            if(row + col == n-1) diagCnt[1].second++;
+        }
         bool chr =  checkRow(row, player);
         bool chc =  checkCol(col, player);
-        bool chd =  checkDiag(row, col, player);
+        bool chd =  checkDiag(player);
 
         if(chr || chc || chd) return player;
         return 0;
