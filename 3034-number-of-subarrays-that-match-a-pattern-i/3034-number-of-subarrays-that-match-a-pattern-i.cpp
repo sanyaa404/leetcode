bd7993ec5f5b5ebe70c89eae4e1546cd31adac55
sanyaa404 <sanya.gera01@gmail.com>
2026-09-23 21:52:00 +0530

    Time: 7 ms (9.36%), Space: 45.1 MB (12.71%) - LeetHub

--- 3034-number-of-subarrays-that-match-a-pattern-i/3034-number-of-subarrays-that-match-a-pattern-i.cpp
+++ 3034-number-of-subarrays-that-match-a-pattern-i/3034-number-of-subarrays-that-match-a-pattern-i.cpp
@@ -1,38 +1,64 @@
 class Solution {
 public:
-    bool check(queue<int> q, vector<int>& v){
-        int i=0;
-        while(!q.empty()){
-            if(i>=v.size() || q.front() != v[i]) return false;
-            q.pop();
-            i++;
+    vector<int> buildLPS(vector<int>& pat){
+        int n = pat.size();
+        vector<int> lps(n,0);
+        int p=0, s=1;
+        while(s<n){
+            if(pat[p] == pat[s]){
+                lps[s] = p+1;
+                p++;
+                s++;
+            }
+            else{
+                if(p == 0){
+                    lps[s] = 0;
+                    s++;
+                }else{
+                    p = lps[p-1];
+                }
+            }
         }
-        return true;
+        return lps;
     }
+
     int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
         int n = nums.size();
         int m = pattern.size();
         if(n < m+1) return 0;
 
-        int l = 0;
-        queue<int> patCaptured;
-        int cnt = 0;
+        int cnt=0;
 
-        for(int r=1; r<n; r++){
-            if(nums[r] > nums[r-1]){
-                patCaptured.push(1);
-            }else if(nums[r] == nums[r-1]){
-                patCaptured.push(0);
+        vector<int> match;
+        for(int i=1; i<n; i++){
+            if(nums[i] > nums[i-1]){
+                match.push_back(1);
+            }else if(nums[i] == nums[i-1]){
+                match.push_back(0);
             }else{
-                patCaptured.push(-1);
-            }
-
-            if(r-l+1 == (m+1)){
-                cnt += check(patCaptured, pattern);
-                patCaptured.pop();
-                l++;
+                match.push_back(-1);
             }
         }
+
+        vector<int> lps = buildLPS(pattern);
+
+        int i=0;
+        int j=0;
+
+        while(i<match.size()){
+            if(match[i] == pattern[j]){
+                i++;
+                j++;
+                if(j==m){
+                    cnt++;
+                    j = lps[j-1];
+                }
+            }else{
+                if(j!=0) j = lps[j-1];
+                else i++;
+            }
+        }
+
         return cnt;
     }
 };
